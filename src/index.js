245a5a77af274ad861e8fb6741f22c76b10e83f1
ferfox88 <ferfox@yandex.ru>
2022-06-26 16:17:30 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = (Math.abs(n)).toString();
+    let num = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        num = num + str[i];
+    }
+    return Number(num);
 }
